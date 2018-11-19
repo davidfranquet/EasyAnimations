a51a09e481eb2db6734d9b7980e9008d3f171473
David Franquet <david@wearemobilefirst.com>
2018-11-19 22:24:22 +0100

    Improve ConstraintLayout animations

--- app/src/main/java/com/weraremobilefirst/animations/splash/SplashActivity.kt
+++ app/src/main/java/com/weraremobilefirst/animations/splash/SplashActivity.kt
@@ -1,10 +1,12 @@
 package com.weraremobilefirst.animations.splash
 
+import android.animation.LayoutTransition
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.constraint.ConstraintSet
-import android.transition.ChangeBounds
-import android.transition.TransitionManager
+import android.transition.*
+import android.view.View
+import android.view.ViewGroup
 import android.view.animation.AnticipateOvershootInterpolator
 import com.weraremobilefirst.animations.R
 import kotlinx.android.synthetic.main.activity_splash.*
@@ -30,10 +32,29 @@ class SplashActivity : AppCompatActivity() {
             val transition = ChangeBounds()
             transition.interpolator = AnticipateOvershootInterpolator(1.0f)
             transition.duration = 1000
+            transition.addListener(object : Transition.TransitionListener{
+                override fun onTransitionEnd(p0: Transition?) {
+                    if(animated){
+                        companyName.animate().alpha(1f)
+                        email.animate().alpha(1f)
+                        password.animate().alpha(1f)
+                    }
+                }
+                override fun onTransitionResume(p0: Transition?) {}
+                override fun onTransitionPause(p0: Transition?) {}
+                override fun onTransitionCancel(p0: Transition?) {}
+                override fun onTransitionStart(p0: Transition?) {
+                    if(!animated){
+                        companyName.animate().alpha(0f)
+                        email.animate().alpha(0f)
+                        password.animate().alpha(0f)
+                    }
+                }
 
+            })
 
             TransitionManager.beginDelayedTransition(rootConstraint, transition)
-            constraintSet.clear(R.id.logoImage, ConstraintSet.BOTTOM)
+            constraintSet.connect(R.id.logoImage, ConstraintSet.BOTTOM, R.id.guideline, ConstraintSet.TOP)
             constraintSet.connect(R.id.bottomBox, ConstraintSet.TOP, R.id.guideline, ConstraintSet.TOP)
 
             if(!animated){
