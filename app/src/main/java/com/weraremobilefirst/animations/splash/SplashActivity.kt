331be1ae568254835bb7bf7c5cf32a361feae22b
David Franquet <david@wearemobilefirst.com>
2018-11-20 20:43:10 +0100

    Refactor

--- app/src/main/java/com/weraremobilefirst/animations/splash/SplashActivity.kt
+++ app/src/main/java/com/weraremobilefirst/animations/splash/SplashActivity.kt
@@ -1,21 +1,21 @@
 package com.weraremobilefirst.animations.splash
 
-import android.animation.LayoutTransition
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.constraint.ConstraintSet
-import android.transition.*
-import android.view.View
-import android.view.ViewGroup
+import android.support.v7.app.AppCompatActivity
+import android.transition.ChangeBounds
+import android.transition.Transition
+import android.transition.TransitionManager
 import android.view.animation.AnticipateOvershootInterpolator
 import com.weraremobilefirst.animations.R
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
-    var animated = false
+    private var animated = false
 
-    val constraintSet = ConstraintSet()
-    val constraintReset = ConstraintSet()
+    private val constraintSet = ConstraintSet()
+    private val constraintReset = ConstraintSet()
+    private val transition = ChangeBounds()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,33 +25,11 @@ class SplashActivity : AppCompatActivity() {
         constraintSet.clone(rootConstraint)
         constraintReset.clone(rootConstraint)
 
+        initializeTransition()
+
         animateButton.setOnClickListener {
 
             animated = !animated
-
-            val transition = ChangeBounds()
-            transition.interpolator = AnticipateOvershootInterpolator(1.0f)
-            transition.duration = 1000
-            transition.addListener(object : Transition.TransitionListener{
-                override fun onTransitionEnd(p0: Transition?) {
-                    if(animated){
-                        companyName.animate().alpha(1f)
-                        email.animate().alpha(1f)
-                        password.animate().alpha(1f)
-                    }
-                }
-                override fun onTransitionResume(p0: Transition?) {}
-                override fun onTransitionPause(p0: Transition?) {}
-                override fun onTransitionCancel(p0: Transition?) {}
-                override fun onTransitionStart(p0: Transition?) {
-                    if(!animated){
-                        companyName.animate().alpha(0f)
-                        email.animate().alpha(0f)
-                        password.animate().alpha(0f)
-                    }
-                }
-
-            })
 
             TransitionManager.beginDelayedTransition(rootConstraint, transition)
             constraintSet.connect(R.id.logoImage, ConstraintSet.BOTTOM, R.id.guideline, ConstraintSet.TOP)
@@ -68,5 +46,33 @@ class SplashActivity : AppCompatActivity() {
 
 
 
+    }
+
+    private fun initializeTransition() {
+        transition.interpolator = AnticipateOvershootInterpolator(1.0f)
+        transition.duration = 1000
+        transition.addListener(object : Transition.TransitionListener {
+            override fun onTransitionEnd(p0: Transition?) {
+                if (animated) {
+                    companyName.animate().alpha(1f)
+                    email.animate().alpha(1f)
+                    password.animate().alpha(1f)
+                }
+            }
+
+            override fun onTransitionStart(p0: Transition?) {
+                if (!animated) {
+                    companyName.animate().alpha(0f)
+                    email.animate().alpha(0f)
+                    password.animate().alpha(0f)
+                }
+            }
+
+            override fun onTransitionResume(p0: Transition?) {}
+            override fun onTransitionPause(p0: Transition?) {}
+            override fun onTransitionCancel(p0: Transition?) {}
+
+
+        })
     }
 }
