cd8f07a29c743962541d5a58127cf74838842fd5
David Franquet <david@wearemobilefirst.com>
2018-12-28 15:33:39 +0100

    Motion layout example

---
+++ app/src/main/java/com/weraremobilefirst/animations/motionlayout/MotionLayoutActivity.kt
@@ -0,0 +1,13 @@
+package com.weraremobilefirst.animations.motionlayout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.weraremobilefirst.animations.R
+
+class MotionLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motion_layout)
+    }
+}
