bcff8b65422d2722546f32526ff39282d6080f55
David Franquet <david@wearemobilefirst.com>
2019-03-13 10:11:04 +0100

    Profile animation with MotionLayout

---
+++ app/src/main/java/com/weraremobilefirst/animations/motionlayout/ProfileMotionLayoutActivity.kt
@@ -0,0 +1,13 @@
+package com.weraremobilefirst.animations.motionlayout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.weraremobilefirst.animations.R
+
+class ProfileMotionLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile_motion_layout)
+    }
+}
